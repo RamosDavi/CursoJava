a8ff65d1a5696a590c20a4e4596195209e2c421f
Davi Ramos <davi.r95@gmail.com>
2024-11-10 03:39:35 -0300

    Exercicio 9
    Finalmente entendi a logica do exercicio 9. Finalizado

--- Eclipse/Curso-java-basico/src/davi/variaveis/cursojava/exercicios2/Exercicio09.java
+++ Eclipse/Curso-java-basico/src/davi/variaveis/cursojava/exercicios2/Exercicio09.java
@@ -38,7 +38,24 @@ public class Exercicio09 {
 			 //valor3 maior
 			 //valor2 < valor1 <valor3
 			 System.out.println(valor3 + " - " + valor1 + " - " + valor2);
+			 
+		 } else if (valor2 <= valor1 && valor2 <= valor3 && valor3 <= valor1) {
+		 // valor2 menor
+		 // valor1 maior
+	     // valor2 < valor3 < valor1
+			 System.out.println(valor1 + " - " + valor3 + " - " + valor2);
+		
+		 } else if (valor3 <= valor1 && valor3 <= valor2 && valor1 <=valor2) {
+			 //valor3 menor
+			 //valor2 maior
+			 //valor3 < valor1 < valor2
+			 System.out.println(valor3 + " - " + valor1 + " - " + valor2);
+			 
+		 } else if (valor3 <= valor1 && valor3 <= valor2 && valor2 <= valor1) {			 
+			 //valor3 menor
+			 //valor1 maior
+			 //valor3 < valor2 < valor1
+			 System.out.println(valor1 + " - " + valor2 + " - " + valor3);
 		 }
-	            
 	}
 }
